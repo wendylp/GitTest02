aea26c583cab522aa634e4a65ac24487a0a5c7f7
wendylp <wendylp@yahoo.com>
2017-06-20 23:38:01 +0800

    version2.0

--- src/com/atguigu/git/HelloGit.java
+++ src/com/atguigu/git/HelloGit.java
@@ -5,6 +5,12 @@ public class HelloGit {
 	public static void main(String[] args) {
 		
 		System.out.println("hello GitHub...");
+		
+
+		System.out.println("hello GitHub ！！！！！！！！！！！！！");
+		
+
+		System.out.println("hello GitHub **************");
 	}
 
 }
